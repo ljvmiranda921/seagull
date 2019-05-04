aeb780888fd1bd429fc7866b2217b9cb1c9eab94
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-04 11:27:08 +0800

    Add board to __init__

--- seagull/__init__.py
+++ seagull/__init__.py
@@ -2,6 +2,10 @@
 
 """Seagull"""
 
-__version__ = "1.0.0-alpha"
+from .board import Board
+
+__all__ = ["Board"]
+
+__version__ = "1.0.0-alpha.1"
 __author__ = "Lester James V. Miranda"
 __email__ = "ljvmiranda@gmail.com"
