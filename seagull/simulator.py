50997dac1a175a6deb4ed14ee05a79ba5d828efa
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-04 22:06:11 +0800

    Add animation for Simulator

--- seagull/simulator.py
+++ seagull/simulator.py
@@ -3,7 +3,10 @@
 # Import standard library
 from typing import Callable
 
+# Import modules
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import animation
 
 from .board import Board
 
@@ -20,33 +23,72 @@ class Simulator:
         self.board = board
         self.history = []
 
-    def run(self, rule: Callable, iters: int, save_history: False):
-        """Run the simulation
+    def run(self, rule: Callable, iters: int) -> np.ndarray:
+        """Run the simulation for a given number of iterations
 
         Parameters
         ----------
         rule : callable
             Callable that takes in an array and returns an array of the same
-            shape
+            shape.
         iters : int
-            Number of iterations to run the simulation. You can pass `-1` for
-            an infinite stream.
-        save_history: bool
-            Save history as a list of arrays. You can access the history
-            through the `self.history` attribute.
-        """
-
-        for i in range(iters):
-            self.board.state = rule(self.board.state)
-            if save_history:
-                self.history.append(self.board.state)
-
-    def get_history(self) -> np.array:
-        """Get simulation history
+            Number of iterations to run the simulation.
 
         Returns
         -------
-        np.array
-            Array of shape `(iters, (board.size))``
+        numpy.ndarray
+            Simulation history
         """
+        for i in range(iters):
+            self.board.state = rule(self.board.state)
+            self.history.append(self.board.state)
+
+        return self.get_history()
+
+    def get_history(self) -> np.ndarray:
         return np.asarray(self.history)
+
+    def animate(self, figsize=(5, 5), interval=100) -> animation.FuncAnimation:
+        """Animate the resulting simulation
+
+        Parameters
+        ----------
+        figsize : tuple
+            Size of the output figure
+        interval : int
+            Interval for transitioning between frames
+
+        Returns
+        -------
+        matplotlib.animation.FuncAnimation
+            Animation generated from the run
+        """
+        if not self.history:
+            raise ValueError("The run() argument must be executed first")
+
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frameon=False)
+        X_blank = np.zeros(self.board.size, dtype=bool)
+        im = ax.imshow(X_blank, cmap=plt.cm.binary, interpolation="nearest")
+        im.set_clim(-0.05, 1)
+
+        def _animate(i, history):
+            current_pos = history[i]
+            im.set_data(current_pos)
+            return (im,)
+
+        def _init():
+            im.set_data(X_blank)
+            return (im,)
+
+        history = self.get_history()
+        anim = animation.FuncAnimation(
+            fig,
+            func=_animate,
+            frames=range(history.shape[0]),
+            init_func=_init,
+            interval=interval,
+            fargs=(history,),
+            blit=True,
+        )
+        return anim
