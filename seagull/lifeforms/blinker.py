a5206d7fa49370ff02105ea7bbb0ca6b3a5cf669
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-04 10:24:42 +0800

    Refactor automatons into lifeforms
    I think using lifeforms instead of automatons sounds better and endows
    the whole thing more with life rather than mechanics.
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- seagull/lifeforms/blinker.py
+++ seagull/lifeforms/blinker.py
@@ -6,11 +6,11 @@ from typing import Dict, Tuple
 # Import modules
 import numpy as np
 
-from .base import Automaton
+from .base import Lifeform
 
 
-class Blinker(Automaton):
-    """A horizontal Blinker automaton"""
+class Blinker(Lifeform):
+    """A horizontal Blinker lifeform"""
 
     def __init__(self, length: int):
         """Initialize the class
