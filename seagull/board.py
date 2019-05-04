e65f5f45508c8b7fcc8ec9953892a1c2cda4f69f
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-04 11:26:54 +0800

    Add add() method to Board

--- seagull/board.py
+++ seagull/board.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 # Import standard library
-from typing import Tuple, Union
+from typing import Tuple
 
 # Import modules
+import matplotlib.pyplot as plt
 import numpy as np
-from loguru import logger
-from matplotlib.axes._subplots import (Axes, Subplot)
+from matplotlib.image import AxesImage
 from scipy.sparse import lil_matrix
 
 from .lifeforms.base import Lifeform
@@ -27,8 +27,7 @@ class Board:
         self.size = size
         self.state = lil_matrix(size, dtype=bool)
 
-    @logger.catch
-    def add(self, lifeform: Lifeform, loc: Tuple[float, float]):
+    def add(self, lifeform: Lifeform, loc: Tuple[int, int]):
         """Add a lifeform to the board
 
         Parameters
@@ -36,25 +35,30 @@ class Board:
         lifeform: seagull.Lifeform
             A lifeform that can evolve in the board
         loc : array_like of size 2
-            Initial location of the lifeform
+            Initial location of the lifeform on the board
         """
-        # TODO: get the size of the lifeform from its property, and use that
-        # for setting the values. Some gotchas you need to consider:
-        # * Cases when the size is just 1-dimensional
-        # * Raise an error whenever the resulting dimension exceeds that of the
-        #   board
-        pass
+        row, col = loc
+        height, width = lifeform.size
+        self.state[row : row + height, col : col + width] = lifeform.layout
 
     def clear(self):
         """Clear the board and remove all lifeforms"""
         self.state = lil_matrix(self.size, dtype=bool)
 
-    def view(self) -> Union[Axes, Subplot]:
+    def view(self, figsize=(5, 5)) -> AxesImage:
         """View the current state of the board
 
         Returns
         -------
-        matplotlib.axes._subplots.AxesSubplot
+        matplotlib.image.AxesImage
             Graphical view of the board
+        figsize : tuple
+            Size of the output figure
         """
-        pass
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frameon=False)
+        im = ax.imshow(
+            self.state.toarray(), cmap=plt.cm.binary, interpolation="nearest"
+        )
+        im.set_clim(-0.05, 1)
+        return im
