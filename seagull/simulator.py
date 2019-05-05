772213da23fe94ab05c3e222bdfc09df315303da
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-05 13:20:30 +0800

    Add statistics module

--- seagull/simulator.py
+++ seagull/simulator.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # Import standard library
-from typing import Callable
+from typing import Callable, Union
 
 # Import modules
 import matplotlib.pyplot as plt
 import numpy as np
+from loguru import logger
 from matplotlib import animation
 
 from .board import Board
+from .utils import statistics as stats
 
 
 class Simulator:
@@ -22,8 +24,9 @@ class Simulator:
         """
         self.board = board
         self.history = []
+        self.stats = {}
 
-    def run(self, rule: Callable, iters: int) -> np.ndarray:
+    def run(self, rule: Callable, iters: int) -> dict:
         """Run the simulation for a given number of iterations
 
         Parameters
@@ -36,14 +39,49 @@ class Simulator:
 
         Returns
         -------
-        numpy.ndarray
-            Simulation history
+        dict
+           Computed statistics for the simulation run
         """
+        # Run simulation
         for i in range(iters):
             self.board.state = rule(self.board.state)
             self.history.append(self.board.state)
 
-        return self.get_history()
+        self.stats = self.compute_statistics(self.get_history())
+        return self.stats
+
+    def compute_statistics(self, history: Union[list, np.ndarray]) -> dict:
+        """Compute various statistics for the board
+
+        Parameters
+        ----------
+        history : list or numpy.ndarray
+            The simulation history
+        stats : dict
+
+
+        Returns
+        -------
+        dict
+            Compute statistics
+        """
+        logger.info("Computing simulation statistics...")
+        sim_stats = {
+            "peak_cell_coverage": np.max(
+                [stats.cell_coverage(h) for h in history]
+            ),
+            "avg_cell_coverage": np.mean(
+                [stats.cell_coverage(h) for h in history]
+            ),
+            "avg_shannon_entropy": np.mean(
+                [stats.shannon_entropy(h) for h in history]
+            ),
+            "peak_shannon_entropy": np.max(
+                [stats.shannon_entropy(h) for h in history]
+            ),
+        }
+
+        return sim_stats
 
     def get_history(self) -> np.ndarray:
         return np.asarray(self.history)
@@ -64,8 +102,11 @@ class Simulator:
             Animation generated from the run
         """
         if not self.history:
-            raise ValueError("The run() argument must be executed first")
+            msg = "The run() argument must be executed first"
+            logger.error(msg)
+            raise ValueError(msg)
 
+        logger.info("Rendering animation...")
         fig = plt.figure(figsize=figsize)
         ax = fig.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frameon=False)
         X_blank = np.zeros(self.board.size, dtype=bool)
---
+++ seagull/utils/statistics.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def shannon_entropy(state: np.ndarray) -> float:
+    """Compute for the shannon entropy for the whole board
+
+    Parameters
+    ----------
+    state : np.ndarray
+        The board state to compute statistics from
+
+    Returns
+    -------
+    float
+        Shannon entropy
+    """
+    zero_probs = np.count_nonzero(state) / np.product(state.shape)
+    one_probs = 1 - zero_probs
+    return -np.sum(np.log2([zero_probs, one_probs]))
+
+
+def cell_coverage(state: np.ndarray) -> float:
+    """Compute for the live cell coverage for the whole board
+
+    Parameters
+    ----------
+    state : np.ndarray
+        The board state to compute statistics from
+
+    Returns
+    -------
+    float
+        Cell coverage
+    """
+    return np.sum(state) / np.product(state.shape)
