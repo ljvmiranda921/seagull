0017514853d0757b891f782da6914eb9412069ec
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-05 15:36:50 +0800

    Update rule imports
    This commit updates how convolve2d is being imported inside the rules
    module. In this case, we are importing scipy.signal rather than
    importing the whole convolve2d itself.
    
    The reason for this is that during tests, we just want to import all
    exposed functions and run similar tests on them. When we do the latter
    version we always get the convolve2d, and this might cause some
    unexpected errors.
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- seagull/rules.py
+++ seagull/rules.py
@@ -2,12 +2,12 @@
 
 # Import modules
 import numpy as np
-from scipy.signal import convolve2d
+import scipy.signal
 
 
-def conway_classic(X):
+def conway_classic(X) -> np.ndarray:
     """The classic Conway's Rule for Game of Life"""
     nbrs_count = (
-        convolve2d(X, np.ones((3, 3)), mode="same", boundary="wrap") - X
+        scipy.signal.convolve2d(X, np.ones((3, 3)), mode="same", boundary="wrap") - X
     )
     return (nbrs_count == 3) | (X & (nbrs_count == 2))
