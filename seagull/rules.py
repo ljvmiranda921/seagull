3c2e97002ab00b9a43b3368e6dae933c130cc9eb
Lester James V. Miranda <ljvmiranda@gmail.com>
2020-04-17 11:19:12 +0800

    Add generalized version of rules (#19)
    We're now adding something like a life_rule that takes in a rulestring.
    TODO: Abstract conways_rule once it passes.
    
    Signed-off-by: Lester James V. Miranda <ljvmiranda@gmail.com>

--- seagull/rules.py
+++ seagull/rules.py
@@ -4,9 +4,15 @@
 Seagull, rules are implemented as a function that takes in a 2-dimensional
 array of a given shape then returns the updated array with the rule applied"""
 
+# Import standard library
+from typing import Tuple, List
+
 # Import modules
 import numpy as np
-import scipy.signal
+from scipy.signal import convolve2d
+from loguru import logger
+
+# Import from package
 
 
 def conway_classic(X) -> np.ndarray:
@@ -20,6 +26,53 @@ def conway_classic(X) -> np.ndarray:
 
     """
     nbrs_count = (
-        scipy.signal.convolve2d(X, np.ones((3, 3)), mode="same", boundary="wrap") - X
+        convolve2d(X, np.ones((3, 3)), mode="same", boundary="wrap") - X
     )
     return (nbrs_count == 3) | (X & (nbrs_count == 2))
+
+
+def life_rule(X: np.ndarray, rulestring: str) -> np.ndarray:
+    """A generalized life rule that accepts a rulestring in B/S notation
+
+    Rulestrings are commonly expressed in the B/S notation where B (birth) is a
+    list of all numbers of live neighbors that cause a dead cell to come alive,
+    and S (survival) is a list of all the numbers of live neighbors that cause
+    a live cell to remain alive.
+
+    Parameters
+    ----------
+    X : np.ndarray
+        The input board matrix
+    rulestring : str
+        The rulestring in B/S notation
+
+    Returns
+    -------
+    np.ndarray
+        Updated board after applying the rule
+    """
+    birth, survival = _parse_rulestring(rulestring)
+    neighbors = _count_neighbors(X)
+    birth_rule = (X == 0) & (neighbors in birth)
+    survival_rule = (X == 1) & (neighbors in survival)
+    return birth_rule | survival_rule
+
+
+def _parse_rulestring(r: str) -> Tuple[List[int], List[int]]:
+    """Parse a rulestring"""
+    try:
+        birth, survival = r.split("/")
+        birth_neighbors = [int(s) for s in birth if s.isdigit()]
+        survival_neighbors = [int(s) for s in survival if s.isdigit()]
+    except Exception as e:
+        msg = f"Cannot parse rulestring {r}: {e}"
+        logger.error(msg)
+        print(msg)
+        raise
+    return birth_neighbors, survival_neighbors
+
+
+def _count_neighbors(X: np.ndarray) -> np.ndarray:
+    """Get the number of neighbors in a binary 2-dimensional matrix"""
+    n = convolve2d(X, np.ones((3, 3)), mode="same", boundary="wrap") - X
+    return n
