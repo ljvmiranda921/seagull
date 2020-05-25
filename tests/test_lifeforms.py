0c946831f59988aeb79a5d162f42c65b03b22821
ep <eugen.pt@gmail.com>
2020-05-25 17:57:37 +0700

    +tests, with files

--- tests/test_lifeforms.py
+++ tests/test_lifeforms.py
@@ -51,3 +51,49 @@ def test_custom_validate_input_shape():
     """Test if error is raised when input array has wrong dimensions"""
     with pytest.raises(ValueError):
         sg.lifeforms.Custom(np.random.choice([0, 1], size=(1, 3, 3)))
+
+def test_parse_plaintext_layout():
+    """Test if parse_plaintext_layout parses sample text correctly"""
+    layout = sg.lifeforms.utils.parse_plaintext_layout('''!Name: Name
+! comment
+..O
+.O
+.
+''')
+    assert len(layout.shape) == 2
+    assert layout.shape==(3,3)
+    assert np.sum(layout - [[0,1,0],[0,0,1],[1,1,1]]) == 0
+
+def test_glider_lifeform(lifeform):
+    """Test if lifeform is a proper Glider"""
+    assert len(lifeform.size) == 2
+    assert lifeform.size==(3,3)
+    assert np.sum(lifeform.layout - [[0,1,0],[0,0,1],[1,1,1]]) == 0
+    assert lifeform.name == 'Glider'
+    assert lifeform.author == 'Richard K. Guy'
+
+def test_lifeform_parse_cells():
+    """Test if lifeform is properly parsed from text"""
+    lifeform = sg.lifeforms.utils.parse_cells('''!Name: Glider
+!Author: Richard K. Guy
+!The smallest, most common, and first discovered spaceship.
+!www.conwaylife.com/wiki/index.php?title=Glider
+.O
+..O
+OOO''')
+    test_glider_lifeform(lifeform)
+
+def test_lifeform_parse_cells_file():
+    """Test if lifeform is properly parsed from file"""
+    lifeform = sg.lifeforms.utils.parse_cells(
+        'glider.cells')
+    test_glider_lifeform(lifeform)
+
+
+def test_lifeform_parse_cells_url():
+    """Test if lifeform is properly parsed from URL"""
+    lifeform = sg.lifeforms.utils.parse_cells(
+        'http://www.conwaylife.com/patterns/glider.cells')
+    test_glider_lifeform(lifeform)
+
+
